e3fe6152216aa9bc33c09c77181de0a3e6f3a83f
Barusu <bodnarbohdan2005@gmail.com>
2025-11-22 12:46:44 +0200

    Refine unit tests and CMake integration

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -16,3 +16,12 @@ TEST(MathOperationsTest, HandlesZero) {
     EXPECT_EQ(add(0, 5), 5);
     EXPECT_EQ(add(7, 0), 7);
 }
+
+TEST(MathOperationsTest, HandlesLargeNumbers) {
+    EXPECT_EQ(add(1'000'000'000, 500'000'000), 1'500'000'000);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
